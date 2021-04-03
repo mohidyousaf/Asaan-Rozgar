5cd1f3aad1fbd246d8672fa96f3906bf75588c1e
Mohid Yousaf <22100201@lums.edu.pk>
2021-04-04 01:13:13 +0500

    adding assets,liabilities,account setup and equity

---
+++ lib/AddLoan-2.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'DataBase.dart';
+
+
+void main() {
+  // TestWidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      home: AddAssets()
+  ));
+}
+
+class AddAssets extends StatefulWidget {
+  @override
+  _AddAssetsState createState() => _AddAssetsState();
+}
+
+class _AddAssetsState extends State<AddAssets> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: Column(
+        children: <Widget>[
+          SizedBox(height:50),
+          Center(
+            child: Text(
+              'Add Loan',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'lato',
+                  color: Colors.black87
+              ),
+            ),
+          ),
+
+          SizedBox(height: 30),
+          Image.asset('assets/loan.png'),
+          SizedBox(height: 30),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 50),
+              color: Colors.white,
+
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      // controller: userEntry,
+                      decoration: InputDecoration(
+                          labelText: 'Type Of Interest',
+                          labelStyle: TextStyle(
+                              color: Colors.grey
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      // controller: userEntry,
+                      decoration: InputDecoration(
+                          labelText: 'Repayment Mode',
+                          labelStyle: TextStyle(
+                              color: Colors.grey
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      // controller: userEntry,
+                      decoration: InputDecoration(
+                          labelText: 'Late Fee (Optional)',
+                          labelStyle: TextStyle(
+                              color: Colors.grey
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 10),
+
+                    SizedBox(height: 40),
+                    Container(
+                      height: 40.0,
+                      width: 200.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.grey[900],
+                        color: Color.fromRGBO(11, 71, 109, 1.0),
+                        elevation: 7.0,
+                        child: TextButton(
+                          onPressed: () {
+                            // var temp = await DBprovider.db.login(userEntry.text.toString(), passEntry.text.toString());
+                            // setState(() {
+                            //   response = temp;
+                            // });
+                          },
+                          child: Center(
+                            child: Text(
+                                'Add Loan',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'lato'
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
