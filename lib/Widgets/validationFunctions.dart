64ab8689133572b58b846101d94468da36430093
adilaslam7 <adilasalmch@hotmail.com>
2021-04-08 02:10:18 +0500

    Restructured code, set routes, connected menu

---
+++ lib/Widgets/validationFunctions.dart
@@ -0,0 +1,19 @@
+
+class ValidationFunctions {
+  static String validateAccount({String text}){
+    if (text.isEmpty){
+      return "Invalid Account No";
+    }
+    else{
+      return null;
+    }
+  }
+  static String validateEmpty({String text}){
+    if (text.isEmpty){
+      return "field empty";
+    }
+    else{
+      return null;
+    }
+  }
+}
