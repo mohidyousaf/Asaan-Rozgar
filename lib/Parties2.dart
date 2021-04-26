b5a9b9ddbd315f736b6dd90fe8d8c826c7836987
adilaslam7 <adilasalmch@hotmail.com>
2021-04-26 17:45:47 +0500

    fixed refresh error

--- lib/Parties2.dart
+++ lib/Parties2.dart
@@ -274,15 +274,15 @@ List<ChildButton> buttons = [ChildButton(label: 'Add Party', icon: Icon(Icons.pe
                            ],
                         ),
                         FlatButton(
-                        onPressed: () {
-                            DBprovider.db.updateOrder(
+                        onPressed: () async{
+                            await DBprovider.db.updateOrder(
                                 partyName,
                                 type,
                                 name,
                                 priceController.text.toString(),
                                 payable.toString(),
                                 _value);
-                            Future.delayed(Duration(milliseconds: 400)).then((_)=>
+                            Future.delayed(Duration(milliseconds: 50)).then((_)=>
                                 Navigator.of(context).pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false));
                         },
                         height: 40,
